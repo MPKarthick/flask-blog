77a04116608cf5c4c7958a0a3ddc525666be04fd
Karthick MP <sigmasix06@gmail.com>
2017-08-06 23:02:26 +0530

    changes in blog and all templates

--- blog.py
+++ blog.py
@@ -5,6 +5,10 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
 
 # create an object for flask app
 app = Flask(__name__)
@@ -17,14 +21,31 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def login():
-    return render_template('login.html')
+    # return render_template('login.html')
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try Again'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error = error), status_code
+
 
 @app.route('/main')
 def main():
     return render_template('main.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('You are logged out')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
