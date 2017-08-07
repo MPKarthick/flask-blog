05a11b4831f991d56bdaa4ce27cf697af4857ebb
Karthick MP <sigmasix06@gmail.com>
2017-08-07 23:51:32 +0530

    Updated blog, template and CSS

--- blog.py
+++ blog.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
+
 
 # configuration
 DATABASE = 'blog.db'
@@ -33,13 +35,60 @@ def login():
             status_code = 401
         else:
             session['logged_in'] = True
+            flash("Login Successful...!")
             return redirect(url_for('main'))
     return render_template('login.html', error = error), status_code
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args,**kwargs):
+        if 'logged_in' in session:
+            return test(*args,**kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)',[request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
+@app.route('/delete',methods=['POST'])
+@login_required
+def delete():
+    title = request.form['title']
+    if not title:
+        flash("Please provide title value")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('delete from posts where title = ?',[request.form['title']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [[dict(title=row[0], post=row[1]) for row in cur.fetchall()]]
+    g.db.close()
+    # print(posts)
+    return render_template('main.html', posts = posts)
 
 @app.route('/logout')
 def logout():
@@ -47,5 +96,6 @@ def logout():
     flash('You are logged out')
     return redirect(url_for('login'))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
